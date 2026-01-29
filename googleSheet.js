c5b46f016bef21a302f2c407a9e64370b905cf2e
frankviolettegy418-jpg <frankviolettegy418@gmail.com>
2026-01-29 22:32:26 +0700

    Update googleSheet.js

--- googleSheet.js
+++ googleSheet.js
@@ -1,7 +1,6 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 const { JWT } = require('google-auth-library')
 
-// Code tự động lấy JSON từ biến môi trường
 const SHEET_ID = process.env.SHEET_ID
 const GOOGLE_CREDENTIALS = process.env.GOOGLE_CREDENTIALS
 
@@ -10,7 +9,6 @@ if (!SHEET_ID || !GOOGLE_CREDENTIALS) {
 }
 
 function getDoc() {
-  // Tự động parse cục JSON bạn ném vào
   let creds
   try {
     creds = JSON.parse(GOOGLE_CREDENTIALS)
@@ -27,15 +25,24 @@ function getDoc() {
   return new GoogleSpreadsheet(SHEET_ID, auth)
 }
 
-async function readJobs() {
+async function readData() {
   const doc = getDoc()
   await doc.loadInfo()
 
-  const sheet = doc.sheetsByIndex[0]
-  const rows = await sheet.getRows()
-  return rows
+  // Lấy đúng tên Tab (Quan trọng)
+  const sheetJobs = doc.sheetsByTitle['Log Progress']
+  const sheetTokens = doc.sheetsByTitle['PAGE_TOKEN']
+
+  if (!sheetJobs || !sheetTokens) {
+    throw new Error('Không tìm thấy tab "Log Progress" hoặc "PAGE_TOKEN"')
+  }
+
+  const jobs = await sheetJobs.getRows()
+  const tokens = await sheetTokens.getRows()
+
+  return { jobs, tokens }
 }
 
 module.exports = {
-  readJobs
+  readData
 }
